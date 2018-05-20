ee2728325599447f1ace5cfef37090437080a12f
jayant soni <jayantson1994.dl.com>
2018-05-20 05:53:32 +0000

    Added Function

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -37,7 +37,14 @@ public class Calculator  {
 		
 		return first*second;
 	}
+
+	 public long divFucn(long first, long second){
+                return first/second;
+        }
 	
+	 public long modFucn(long first, long second){
+                return first%second;
+        }
 	public static void main(String[] args) {
 		
 		Calculator cc = new Calculator();
@@ -55,7 +62,8 @@ public class Calculator  {
 		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
-		
+		System.out.println("The sum of two numbers is: "+cc.divFucn(first, second));
+		System.out.println("The sum of two numbers is: "+cc.modFucn(first, second));
 	}
 	 
 
