2395e94b53ab18b775a065e39e5666025b06260e
jayant soni <jayantson1994.dl.com>
2018-05-20 05:56:32 +0000

    Changing Code

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -62,8 +62,8 @@ public class Calculator  {
 		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
-		System.out.println("The sum of two numbers is: "+cc.divFucn(first, second));
-		System.out.println("The sum of two numbers is: "+cc.modFucn(first, second));
+		System.out.println("The Div of two numbers is: "+cc.divFucn(first, second));
+		System.out.println("The Mod of two numbers is: "+cc.modFucn(first, second));
 	}
 	 
 
